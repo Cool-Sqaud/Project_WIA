726468623111b67b3ffa6c32d7078eb609426f47
luckyluuk28 <luukvdstruik.lvds@gmail.com>
2023-03-22 16:16:43 +0100

    Added Login system, waiting for database

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './../_services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+      //Check if logged in already
+  };
+
+  onSubmit(): void {
+    if (!this.loginForm.valid) { 
+      console.log(this.loginForm.value, 'is invalid');
+      return;
+    }
+    this.authService.login(this.loginForm.value);
+  }
 }
