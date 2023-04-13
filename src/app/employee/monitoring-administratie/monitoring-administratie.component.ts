7c84ac8708b226133d1525a71d53642dee4dd9c0
StandeMan <s.freese@st.hanze.nl>
2023-04-13 20:03:20 +0200

    Data presenteren op de pagina

--- src/app/employee/monitoring-administratie/monitoring-administratie.component.ts
+++ src/app/employee/monitoring-administratie/monitoring-administratie.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { measurements } from '../../interfaces';
+
 @Component({
   selector: 'app-monitoring-administratie',
   templateUrl: './monitoring-administratie.component.html',
   styleUrls: ['./monitoring-administratie.component.css']
 })
+
+
 export class MonitoringAdministratieComponent {
+  measurements = measurements;
+
+  share(){
+    window.alert('Succes');
+  }
 
 }
+
